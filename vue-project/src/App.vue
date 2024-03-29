c11c1d7eac3e5397e14f4e310e3a2267c125f2ac
nickshakh <shakhbazyann@gmail.com>
2024-03-29 04:15:53 +0400

    tried interaction with user

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,20 +2,50 @@
 <input type="text" v-model="username" placeholder="Name">
 <input type="password" v-model="userPass" placeholder="Pass">
 <input type="email" v-model="userEmail" name="email" placeholder="Email">
-<p><b> {{ username }} </b> this is name</p>
-<p> <b>{{ userPass }}</b> this is pass</p>
-<p> <b>{{ userEmail }} </b> this is email</p>
+<button @click="sendData()">Send</button>
+<p className="err">{{ err }}</p>
+
+<div v-for="(e,index) in users" :key="index">
+  <h3>{{ e.name }}</h3>
+  <p>{{ e.pass }}, {{ e.email }}</p>
+</div>
+<div v-if="users.length == 0">
+  there are no users
+</div>
+<div v-else-if="users.length == 1">
+  There is only 1 user
+</div>
+
+<div v-else>
+There are users
+</div>
+
 </template>
 
 <script>
   export default {
       data() {
         return {
+         users : [],
+         err:'',
          username : '',
          userPass : '',
          userEmail : ''
         }
       },methods : {
+        sendData() {
+          if(this.username === '' || this.userPass === '' || this.userEmail === '') {
+           this.err = 'Fill every'
+            return
+          }
+          this.err = ''
+
+          this.users.push({
+            name : this.username,
+            pass : this.userPass,
+            email : this.userEmail,
+          })
+        }
       }
   }
 </script>
