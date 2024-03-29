cab36e4015060804c8c39a88390a77ab5eb6e100
nickshakh <shakhbazyann@gmail.com>
2024-03-29 19:20:16 +0400

    Started creating Weather app

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,58 +1,61 @@
 <template>
-<input type="text" v-model="username" placeholder="Name">
-<input type="password" v-model="userPass" placeholder="Pass">
-<input type="email" v-model="userEmail" name="email" placeholder="Email">
-<button @click="sendData()">Send</button>
-<p className="err">{{ err }}</p>
-
-<div v-if="users.length == 0">
-  there are no users
-</div>
-<div v-else-if="users.length == 1">
-  There is only 1 user
-</div>
-<div v-else>
-There are users
-</div>
-
-<User v-for="(el, index) in users" :key="index" :user="el" :deleteUser="deleteUser" :index="index" />
-
+  <div className="wrapper">
+    <h1>The Weather App</h1>
+    <p>Check the weather in your city!</p>
+    <input type="text" placeholder="Type city">
+    <button>Get Inforamtion </button>
+  </div>
 </template>
 
 <script>
-import User from './components/User.vue';
-  export default {
-    components :{User},
-      data() {
-        return {
-         users : [],
-         err:'',
-         username : '',
-         userPass : '',
-         userEmail : ''
-        }
-      },methods : {
-        sendData() {
-          if(this.username === '' || this.userPass === '' || this.userEmail === '') {
-           this.err = 'Fill every'
-            return
-          }
-          this.err = ''
 
-          this.users.push({
-            name : this.username,
-            pass : this.userPass,
-            email : this.userEmail,
-          })
-        },
-        deleteUser(index) {
-          this.users.splice(index,1)
-        }
-      }
-  }
 </script>
 
 <style scoped>
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  background-color: #1f0f24;
+  color: white;
+  padding: 20px;
+  text-align: center;
+}
 
+.wrapper h1 {
+  margin-top: 50px;
+}
 
+.wrapper p {
+  margin-top: 20px;
+}
+
+.wrapper input {
+  margin-top: 30px;
+  background-color: transparent;
+  border: 0px;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding:5px 8px;
+  outline: none;
+}
+.wrapper input:focus {
+  border-bottom-color: #6e2d7d;
+}
+
+.wrapper button {
+  background: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: 500ms ease;
+}
+
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px)
+}
 </style>
